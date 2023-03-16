967f6f4db621abda23532944df052b2ceb8a2e05
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 <alaineakeza12@gmail.com>
2023-03-16 07:19:09 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main -entry point 
+ *
+ * return: Always 0 (success)
+ */
+init main(void)
+{
+	puts("\"programming is like building a multitingual puzzl");return (0)
+}
