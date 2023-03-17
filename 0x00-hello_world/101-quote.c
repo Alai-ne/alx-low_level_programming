e4902fc911989067a2b5b2a1247670d0126b4a1c
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 <alaineakeza12@gmail.com>
2023-03-17 04:44:44 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return:Always 1 (Succes)
- */
+* main - Entry point
+*
+* Return: Always 1 (Succes)
+*/
 int main(void)
 {
 	write(2,
-			"an that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);return (1):
+			"an that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
+	return (1);
 }
