0a356b4f5ae4563f2b8047159211215af8430231
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 <alaineakeza12@gmail.com>
2023-03-20 07:19:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 /**
-*main - entry point
-*My hello, puts task
-*
-*return: always 0 (success)
-*/
+ *main - entry point
+ *return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
