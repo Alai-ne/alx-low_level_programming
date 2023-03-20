f37cc3b1476a061c8c788c6f25e4d465c5453f62
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 <alaineakeza12@gmail.com>
2023-03-20 07:02:44 -0700

    qoute C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 1 (Succes)
-*/
+ *main - Entry point
+ *Discription: 'quote C'
+ *Return: Always 0
+ */
 int main(void)
 {
-	write(2,
-			"an that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
+	write(2, "an that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
